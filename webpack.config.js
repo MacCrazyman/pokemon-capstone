99d03de554be99b0ba5722e037641bcb91c7138b
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2021-12-21 10:29:10 -0500

    Basic footer structure

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
   },
   devtool: 'inline-source-map',
