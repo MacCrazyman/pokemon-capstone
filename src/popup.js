8a47040e98283b26609bd3c538eaf220286d19cf
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2021-12-21 14:29:13 -0500

    Add HTML structure to popup window

---
+++ src/popup.js
@@ -0,0 +1,6 @@
+// fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
+//   method: 'POST',
+// })
+// .then(response => console.log('Success:', response));
+
+const appId = '2VoylRMjGXYqZZMlt91a';
