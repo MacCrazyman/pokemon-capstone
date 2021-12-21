a07999d1f912383dd8c6394cda69f63e5f140b3d
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2021-12-21 17:35:26 -0500

    add basic functions

--- src/popup.js
+++ src/popup.js
@@ -4,3 +4,16 @@
 // .then(response => console.log('Success:', response));
 
 const appId = '2VoylRMjGXYqZZMlt91a';
+const involvementAPI = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}`;
+
+const getComments = () => {
+  return fetch(`${involvementAPI}/comments`).then((Response) => Response.json())
+};
+
+const submitComment = (newComment) => {
+  return fetch(`${involvementAPI}/comments`,{
+    method: 'POST',
+    body: JSON.stringify(newComment),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  })
+}
