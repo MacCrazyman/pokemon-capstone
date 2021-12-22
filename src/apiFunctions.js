8ea361656ee9ae4d7370344f4baec26f3ee94da4
Santiago Velosa <santiagovelosa@gmail.com>
2021-12-22 09:56:30 -0500

    Fix first review comment

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,10 +1,9 @@
 const getPokemon = () => fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0/').then((response) => response.json());
 
 const arrayPokemonLinks = async () => {
-  const data = await getPokemon();
-  const array = data.results;
+  const data = await getPokemon().then((response) => response.results);
   const urlArray = [];
-  array.forEach((element) => {
+  data.forEach((element) => {
     urlArray.push(element.url);
   });
   return urlArray;
