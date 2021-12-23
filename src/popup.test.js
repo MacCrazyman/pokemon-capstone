94b1d6c2c954b39b9f70e9abba15639f650fa812
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2021-12-23 11:59:29 -0500

    Add unit test for Comments counter

---
+++ src/popup.test.js
@@ -0,0 +1,40 @@
+import {countComments} from './popup.js'
+
+
+describe('Testing comments counter', () => {
+
+  test.each([
+    {
+      comments : [
+        {'comment 1': 'new comment'},
+        {'comment 2': 'new comment'},
+        {'comment 3': 'new comment'},
+        {'comment 4': 'new comment'},
+      ], 
+      result : 4
+    },
+    {
+      comments : [
+        {'comment 1': 'new comment'},
+        {'comment 2': 'new comment'},
+        {'comment 3': 'new comment'},
+        {'comment 4': 'new comment'},
+        {'comment 5': 'new comment'},
+      ], 
+      result : 5
+    },
+    {
+      comments : [
+        
+      ], 
+      result : 0
+    },
+
+  ])('Testing array with $result elements',({comments, result}) => {
+    console.log(comments);
+    console.log(result);
+  const commentsNumber = countComments(comments);
+
+  expect(commentsNumber).toBe(result);
+
+})})
